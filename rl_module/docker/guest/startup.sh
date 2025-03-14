1c5643c4226779aa52f79814c98d3a16ad8dc8a3
Dan7171 <evrondan99@gmail.com>
2025-03-14 16:12:24 +0200

    develop from docker with vsc is on. See next commit for readme

--- rl_module/docker/guest/startup.sh
+++ rl_module/docker/guest/startup.sh
@@ -5,4 +5,4 @@ echo "- display exporting: configued successfully V"
 
 cp /host/rl_for_curobo//isaac_sim-4.0.0/exts/omni.isaac.nucleus/omni/isaac/nucleus/nucleus.py ../../isaac-sim/exts/omni.isaac.nucleus/omni/isaac/nucleus/nucleus.py 
 echo "- fast boot of isaac-sim 4.0.0: configued successfully V" # for more info see https://forums.developer.nvidia.com/t/detected-a-blocking-function-this-will-cause-hitches-or-hangs-in-the-ui-please-switch-to-the-async-version/271191/12#:~:text=I%20found%20a,function%20to%20below
-echo "- ready to execute curobo simulations! (example: root/@USERNAME/pkgs/curobo# omni_python ./examples/isaac_sim/mpc_example.py)"
+echo "- ready to execute curobo simulations! (example: root/@USERNAME/pkgs/curoboomni_python ./examples/isaac_sim/mpc_example.py)"
